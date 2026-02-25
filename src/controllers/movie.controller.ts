4026ca1d93b5fe48711baad85180a92a446a45b8
Karim Pierre Zennoune <karim-pierre.zennoune@laplateforme.io>
2026-02-25 15:35:04 +0100

    movie by id add director and collaborators

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -48,9 +48,13 @@ const remove: RequestHandler = async (req, res, next) => {
   }
 };
 
-const getById: RequestHandler = async (_req, res, next) => {
+const getById: RequestHandler = async (req, res, next) => {
   try {
-    const { id } = _req.params;
+    const { id } = req.params;
+    const idAsInt = parseInt(id as string);
+    if (isNaN(idAsInt) || idAsInt <= 0) {
+      throw new AppError(400, 'Wrong query params');
+    }
     const response = await movieService.getById(parseInt(id as string));
     return res.send(response);
   } catch (e) {
